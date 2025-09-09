767030acb9085620e38cd7146e21d199dc52b254
cmliu <cmliu@users.noreply.github.com>
2025-09-09 18:31:38 +0800

    为fetch请求添加User-Agent头，以确保正确获取subConverter响应

--- _worker.js
+++ _worker.js
@@ -57,7 +57,7 @@ export default {
                 }
 
                 try {
-                    const subConverterResponse = await fetch(subConverterUrl);
+                    const subConverterResponse = await fetch(subConverterUrl, { headers: { 'User-Agent': UA } });
 
                     if (!subConverterResponse.ok) {
                         throw new Error(`Error fetching subConverterUrl: ${subConverterResponse.status} ${subConverterResponse.statusText}`);

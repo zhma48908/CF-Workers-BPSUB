5df3a26341e45b37aa0b0c8778e7d97338a8e65a
cmliu <cmliu@users.noreply.github.com>
2025-09-13 19:39:56 +0800

    添加_worker.js文件

---
+++ proxy_host/_worker.js
@@ -0,0 +1,9 @@
+export default {
+    async fetch(request, env, ctx) {
+        if (request.headers.get('Upgrade') !== 'websocket') {
+            return new Response('Access Denied', { status: 403 });
+        }
+        let url = new URL(request.url);
+        return fetch(new Request("https://snippets.neib.cn" + url.pathname, request));
+    }
+};

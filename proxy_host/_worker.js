4d5174c51436a90761249d71c4a640ca7c728ec6
cmliu <cmliu@users.noreply.github.com>
2025-09-19 23:39:12 +0800

    优化代理主机文件，提取主机名为常量并添加自动压缩工作流

--- proxy_host/_worker.js
+++ proxy_host/_worker.js
@@ -1,9 +1,10 @@
+const hostname = "snippets.neib.cn";
 export default {
     async fetch(request, env, ctx) {
         if (request.headers.get('Upgrade') !== 'websocket') {
             return new Response('Access Denied', { status: 403 });
         }
         let url = new URL(request.url);
-        return fetch(new Request("https://snippets.neib.cn" + url.pathname + url.search, request));
+        return fetch(new Request("https://" + hostname + url.pathname + url.search, request));
     }
 };
